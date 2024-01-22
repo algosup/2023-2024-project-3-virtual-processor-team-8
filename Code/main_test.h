7e810ddccabde7eb4c79c51192fe0023dc3862f5
laurentbouquin <laurent.bouquin@algosup.com>
2024-01-22 10:30:15 +0100

    Add unit testing's base.
    Add ing files to start writing unit tests.

---
+++ Code/main_test.h
@@ -0,0 +1,11 @@
+
+// Testing that the file is not empty & that the function gets the data
+int fileDataIsNotNull();
+
+int lineIsNotNull();
+
+int isLineCorrect();
+
+int getFunctionsTest();
+
+int getAttributeTest();
