2a8786957bd6a11ccc582982b9312ba3d511e8d6
laurentbouquin <laurent.bouquin@algosup.com>
2024-02-21 10:40:56 +0100

    Update Virtual_Processor.cmd
    Change the fact that it worked only on my machine.

--- Virtual_Processor.cmd
+++ Virtual_Processor.cmd
@@ -7,8 +7,18 @@ set /p "File_path=Enter your assembly file's path: "
 @echo off
 set Path=C:\MinGW\bin;%PATH%
 
-cd C:\Users\LaurentBOUQUIN\Documents\GitHub\2023-2024-project-3-virtual-processor-team-8\Src
+echo Searching for the directory...
 
+@echo off
+FOR /F "tokens=1" %%G IN ('dir /AD /s /b 2023-2024-project-3-virtual-processor-team-8') DO set Directory=%%G
+
+@echo off
+cd %Directory%
+
+@echo off
+cd Src
+
+@echo off
 echo %File_path% > path.txt
 
 gcc -o main.exe main.c
