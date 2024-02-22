ed93b3c39657d04553bf9fd6cc0688caabe73b0f
Pierre Gorin <pierre.gorin@algosup.com>
2024-02-22 11:34:59 +0100

    Update Virtual_Processor.sh

--- Virtual_Processor.sh
+++ Virtual_Processor.sh
@@ -2,6 +2,11 @@
 
 echo "Enter your assembly file's path: "
 read File_path
+# // if file path is empty set /Users/pierregorin/Documents/GitHub/2023-2024-project-3-virtual-processor-team-8/Src/code.asm as default
+if [ -z "$File_path" ]
+then
+    File_path="/Users/pierregorin/Documents/GitHub/2023-2024-project-3-virtual-processor-team-8/Src/code.asm"
+fi
 
 cd Src
 
