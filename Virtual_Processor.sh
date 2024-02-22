e2581be5f5ddbcfdbb54f3d92bd67568a0fdb4b7
Victor-Leroy <victor.leroy@algosup.com>
2024-02-22 10:14:09 +0100

    update the shell script to make it faster

--- Virtual_Processor.sh
+++ Virtual_Processor.sh
@@ -3,13 +3,6 @@
 echo "Enter your assembly file's path: "
 read File_path
 
-export PATH=/usr/local/bin:$PATH
-
-echo "Searching for the directory..."
-
-Directory=$(find / -type d -name "2023-2024-project-3-virtual-processor-team-8" 2>/dev/null)
-
-cd "$Directory"
 cd Src
 
 echo $File_path > path.txt
