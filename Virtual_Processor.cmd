e51b4effc7cdbc160675a3ac776b74608368563f
laurentbouquin <laurent.bouquin@algosup.com>
2024-02-22 10:03:16 +0100

    Update Virtual_Processor.cmd

--- Virtual_Processor.cmd
+++ Virtual_Processor.cmd
@@ -1,19 +1,5 @@
 @echo off
-cd C:\
-
-@echo off
 set /p "File_path=Enter your assembly file's path: "
-
-@echo off
-set Path=C:\MinGW\bin;%PATH%
-
-echo Searching for the directory...
-
-@echo off
-FOR /F "tokens=1" %%G IN ('dir /AD /s /b 2023-2024-project-3-virtual-processor-team-8') DO set Directory=%%G
-
-@echo off
-cd %Directory%
 
 @echo off
 cd Src
